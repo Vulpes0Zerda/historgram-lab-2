187e88b9ed4962c1849419e6979c1414b2c90e71
johanna seidel <johanna211105@gmail.com>
2025-05-04 12:33:47 +0200

    junit test from charactercounter

---
+++ app/src/test/java/histogram/CharCounterTest.java
@@ -0,0 +1,45 @@
+package histogram;
+
+import histogram.fileIO.FileReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// sudo bash ./gradlew clean build
+public class CharCounterTest {
+
+    private CharacterCounter characterCounter;
+
+    @BeforeEach
+    public void setUp() {
+        characterCounter = new CharacterCounter();
+    }
+
+    @Test
+    public void testCountCharactersFromFile() throws IOException {
+        String filePath = "file.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write("Hello World!");
+        }
+
+        FileReader fileReader = new FileReader(filePath);
+        fileReader.getCharacterStream().forEach(line -> {
+            for (char c : line.toCharArray()) {
+                characterCounter.countCharacter(c);
+            }
+        });
+
+        assertEquals(1, characterCounter.charCount.get('H'));
+        assertEquals(3, characterCounter.charCount.get('l'));
+        assertEquals(2, characterCounter.charCount.get('o'));
+        assertEquals(1, characterCounter.charCount.get('W'));
+        assertEquals(1, characterCounter.charCount.get('r'));
+        assertEquals(1, characterCounter.charCount.get('d'));
+        assertEquals(1, characterCounter.charCount.get('e'));
+    }
+}
